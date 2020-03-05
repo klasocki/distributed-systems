d417f5d49a254aadc2c6bdbc33fffe60c76e4175
klasocki <karolasocki@gmail.com>
2020-03-05 01:57:58 +0100

    Initial Chat server, working TCP

---
+++ Lasocki_Karol_1/ChatClient.py
@@ -0,0 +1,75 @@
+import socket
+import threading
+import sys
+
+
+class ChatClient:
+    server_addr = ("127.0.0.1", 10010)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    using_udp = False
+    buff = []
+    running = False
+
+    def __init__(self):
+        nick = input('Client starting, what is your nick?\n')
+        self.connect(nick)
+        self.receive_tcp = threading.Thread(target=self.wait_for_tcp_messages)
+        self.receive_udp = threading.Thread(target=self.wait_for_udp_messages)
+
+    def start(self):
+        self.running = True
+        self.receive_tcp.start()
+        self.send_messages()
+
+    def connect(self, nick: str):
+        self.tcp_socket.connect(self.server_addr)
+        self._send_message(f'REGISTER {nick}')
+
+    def send_messages(self):
+        while True:
+            msg = input('Your message to others:\n')
+            if msg == 'U':
+                self.using_udp = not self.using_udp
+                print('Switched communication mode, now using', 'UDP' if self.using_udp else 'TCP')
+            else:
+                self._send_message(msg)
+
+    def quit(self):
+        self.running = False
+        self.tcp_socket.send('UNREGISTER'.encode('utf-8'))
+        self.udp_socket.close()
+        self.tcp_socket.close()
+
+    def wait_for_tcp_messages(self):
+        while self.running:
+            buff = self.tcp_socket.recv(4096)
+            self._log_message(buff)
+
+    def wait_for_udp_messages(self):
+        while self.running:
+            buff = self.udp_socket.recv(4096)
+            self._log_message(buff)
+
+    @staticmethod
+    def _log_message(buff):
+        received = str(buff, 'utf-8')
+        print('-' * 50)
+        print(received)
+        print('-' * 50)
+
+    def _send_message(self, msg):
+        msg = msg.encode('utf-8')
+        if self.using_udp:
+            self.udp_socket.sendto(msg, self.server_addr)
+        else:
+            self.tcp_socket.send(msg)
+
+
+if __name__ == '__main__':
+    client = ChatClient()
+    try:
+        client.start()
+    except KeyboardInterrupt:
+        client.quit()
+        sys.exit(0)
---
+++ Lasocki_Karol_1/ChatServer.py
@@ -0,0 +1,67 @@
+import socket
+from concurrent.futures import ThreadPoolExecutor
+
+
+class ChatServer:
+    server_port = 10010
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clients = {}
+    max_clients = 128
+
+    def __init__(self):
+        self.udp_socket.bind(('', self.server_port))
+        self.tcp_socket.bind(('', self.server_port))
+        self.tcp_socket.listen()
+        print('CHAT SERVER STARTING')
+
+    def serve(self):
+        with ThreadPoolExecutor(max_workers=self.max_clients) as executor:
+            executor.submit(self._handle_udp)
+            try:
+                while True:
+                    client, address = self.tcp_socket.accept()
+                    executor.submit(self._handle_client, client, address)
+            except KeyboardInterrupt:
+                self.tcp_socket.close()
+                self.udp_socket.close()
+
+    def _handle_udp(self):
+        buff, sender_addr = self.udp_socket.recvfrom(4096)
+        msg = str(buff, 'cp1250').strip()
+        print(f'Server received message: {msg}')
+        for recipient_addr in self.clients.keys():
+            if recipient_addr != sender_addr:
+                self._send_udp_message(f"From {self.clients[sender_addr][0]}: {msg}", recipient_addr)
+        self._handle_udp()
+
+    def _handle_client(self, client: socket.socket, sender_addr):
+        buff = client.recv(4096)
+        msg = str(buff, 'cp1250').strip()
+        print(f'Server received message: {msg}')
+        if msg.startswith('REGISTER'):
+            nick = msg.split(maxsplit=1)[1]
+            self.clients[sender_addr] = nick, client
+            self._send_tcp_message(f'{nick} registered successfully', client)
+        elif msg.startswith('UNREGISTER'):
+            nick, _ = self.clients.pop(sender_addr)
+            self._send_tcp_message(f'{nick} unregistered successfully', client)
+            client.close()
+            return
+        else:
+            for recipient_addr in self.clients.keys():
+                if recipient_addr != sender_addr:
+                    self._send_tcp_message(f"From {self.clients[sender_addr][0]}: {msg}", self.clients[recipient_addr][1])
+        self._handle_client(client, sender_addr)
+
+    @staticmethod
+    def _send_tcp_message(msg, client: socket.socket):
+        client.send(msg.encode('utf-8'))
+
+    def _send_udp_message(self, msg, address):
+        self.udp_socket.sendto(msg.encode('utf-8'), address)
+
+
+if __name__ == '__main__':
+    server = ChatServer()
+    server.serve()
